3500e304b7a89cbed7b86c29ff20950c11d17c9e
Maxime <maxime.boure@gmail.com>
2021-07-15 15:03:41 +0200

    [application controller] add current user access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+ serialization_scope :current_user
+
+  def require_current_user
+    if current_user
+      return
+    end
+
+    render json: { error: 'you must be logged in' }, status: :unauthorized
+  end
+
+  # To simplify the test, I'm using the first user in database
+  def current_user
+    User.first
+  end
 end
