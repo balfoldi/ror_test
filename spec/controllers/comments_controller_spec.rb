fe97b1b1f5e65128b2cce753e19fb9724ca0fc58
BorisSIUS <balfooldi@yahoo.fr>
2021-11-01 17:04:28 +0100

    Add test to comment controller for serialization upgrade

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,20 +3,39 @@ require 'rails_helper'
 RSpec.describe CommentsController, type: :controller do
 
   describe "GET #index" do
-    let(:comment) { FactoryBot.create(:comment) }
+    let!(:user_logged) { FactoryBot.create(:user) }
+    let(:post) { FactoryBot.create(:post) }
+    let(:comment) { FactoryBot.create(:comment, post: post, user: user_logged) }
+    let!(:comment_liked) { FactoryBot.create(:comment, post: post, user: user_logged) }
+    let!(:like) { FactoryBot.create(:like, user: user_logged, liked: comment_liked) }
     subject { get :index, params: { post_id: comment.post.id } }
 
     it { is_expected.to have_http_status(:success) }
-    it { expect(JSON.parse(subject.body).sample).to include "id", "post", "user", "likes", "content" }
+    it { expect(JSON.parse(subject.body).sample).to include "id", "post", "user", "likes_count", "content", "liked" }
+
+    it "liked key return true if user_logged like" do
+      comment_liked_attributes = JSON.parse(subject.body).find { |comment_attributes| comment_attributes["id"] == comment_liked.id }
+      expect(comment_liked_attributes["liked"]).to be true
+    end
+
+    it "liked key return true if user_logged like" do
+      comment_attributes = JSON.parse(subject.body).find { |comment_attributes| comment_attributes["id"] == comment.id }
+      expect(comment_attributes["liked"]).to be false
+    end
+
+    it "like_count attributes return an integer" do
+      comment_attributes = JSON.parse(subject.body).sample
+      expect(comment_attributes["likes_count"]).to be_a_kind_of(Integer)
+    end
   end
 
   describe "GET #create" do
-    let!(:post_commentable) { FactoryBot.create(:post) }
+    let(:post_commentable) { FactoryBot.create(:post) }
     subject { post :create, params: { post_id: post_commentable.id, comment: { content: Faker::Lorem.characters(number: 10) } } }
 
     it { is_expected.to have_http_status(:success) }
     it { expect { subject }.to change { post_commentable.comments.count } }
-    it { expect(JSON.parse(subject.body)).to include "id", "post", "user", "likes", "content" }
+    it { expect(JSON.parse(subject.body)).to include "id", "post", "user", "likes_count", "content", "liked" }
   end
 
 end
