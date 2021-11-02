1b6357ca5f25b7c1dc9a081b0a58b0fd75c315ed
BorisSIUS <balfooldi@yahoo.fr>
2021-11-02 17:40:11 +0100

    Test comment likes_count auto update

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,4 +3,19 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   it { should belong_to(:user) }
   it { should belong_to(:post) }
+
+  describe "#update_likes_count" do
+    subject { FactoryBot.create(:comment) }
+
+    context "after creation" do
+      it { expect(subject.likes_count).to be 0}
+    end
+
+    context "has multiples likes" do
+      let!(:likes) { FactoryBot.create_list(:like, rand(2..10), liked: subject) }
+
+      before { subject.update(likes_count: 0)}
+      it { expect { subject.update_likes_count }.to change { subject.likes_count }.by(likes.size) }
+    end
+  end
 end
